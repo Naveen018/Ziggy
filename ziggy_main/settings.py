a3820da063bf7833bb354ea371226e822058317b
Naveen018 <70464902+Naveen018@users.noreply.github.com>
2024-09-16 00:42:14 +0530

    Implemented home page template and collected all static files in root dir

--- ziggy_main/settings.py
+++ ziggy_main/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = "ziggy_main.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": ["templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -115,7 +115,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_DIRS = [
+    "ziggy_main/static/"
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- ziggy_main/views.py
+++ ziggy_main/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("<h2>This is home page</h2>")
+    return render(request, "home.html")
