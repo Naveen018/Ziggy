54f8de0aa843c95e415b84c6c81fa92cef403bbe
Naveen018 <70464902+Naveen018@users.noreply.github.com>
2024-10-09 09:15:56 +0530

    User Registration functionality

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib import messages
+
+from accounts.forms import UserForm
+from accounts.models import User
 
 # Create your views here.
+def registerUser(request):
+    if request.method == "POST":
+        # print(request.POST)
+        form = UserForm(request.POST)
+        if form.is_valid():
+            # print("Hiiiiiii")
+            # Create user using the form
+            password = form.cleaned_data["password"]
+            user = form.save(commit=False)
+            user.set_password(password)  #set_password() will set password to hashed format
+            user.role = User.CUSTOMER
+            user.save()
+            messages.success(request, "Your account has been registered successfully!!")
+            return redirect('registerUser')
+        else:
+            messages.error(request, "Please Try Again!!!")
+            print(form.errors)
+    else:
+        form = UserForm()
+    context = {"form":form,}
+    return render(request, "accounts/registerUser.html", context)
